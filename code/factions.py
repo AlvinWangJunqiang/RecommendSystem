3ecf044785c723a0b54410f0898b1b9aeb40f763
BUPTLdy <498435408@qq.com>
2015-12-27 13:40:02 +0800

    add file

--- code/factions.py
+++ code/factions.py
@@ -4,7 +4,8 @@ Created on Sat Dec 26 14:06:08 2015
 
 @author: Ldy
 """
-
+from operator import itemgetter
+import numpy as np
 def calsim(VecA,VecB):
     Vsum=0.
     MAsum=0.
@@ -18,8 +19,37 @@ def calsim(VecA,VecB):
     else:
         sim=Vsum/((MAsum**0.5)*(MBsum**0.5))
     return sim
-    
-
 A=[1,0,1]
 B=[-2,0,-2]
 print calsim(A,B)    
+
+def tagssim(SetA,SetB):
+    sim=0
+    for key in SetA:
+        if key in SetB:
+            sim=sim+1
+    return sim
+
+a=set(A)
+b=set(B)
+print tagssim(a,b)
+
+news_hoter= np.load('../data/news_hoter.npy').item()
+news_hoter=sorted(news_hoter.items(), key=itemgetter(1), reverse=True)
+hotest_100=dict(news_hoter[0:100])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
--- code/readfile.py
+++ code/readfile.py
@@ -1,7 +1,7 @@
-
+import collections
 import pandas as pd
 import time
-
+import numpy as np
 
 def readfile(filename, sep, headers=None, names=None, septime = "2014-03-20 23:59:00"):
     '''
@@ -33,7 +33,20 @@ if __name__ == '__main__':
     sep = '\t'
     names = ['user_id', 'news_id', 'read_time', 'news_title', 'news_content', 'news_publi_time']
     training_data, testing_data = readfile(filename,sep,names=names)
-    print training_data['news_title']
-
-
+    tran_news_id= list(training_data['news_id'].values)
+    test_news_id= list(testing_data['news_id'].values)
+    news_id_list=tran_news_id+test_news_id
+    #print len(news_id_list)
+    test_user=set(list(testing_data['user_id'].values))
+    tran_user=set(list(training_data['user_id'].values))
+    same_user=set()
+    for user in test_user:
+        if user in tran_user:
+            same_user.add(user)
+    print same_user,len(same_user),len(test_user)
+    np.save('../data/same_user.npy', same_user)
+    counter=collections.Counter(news_id_list)
+    news_hoter=dict(counter)
+    #print news_hoter[100648598]
+    np.save('news_hoter.npy', news_hoter)
 
